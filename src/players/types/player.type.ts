4d14075be1430beb6e39fb48229be047fec27567
William Koller <wkoller25@gmail.com>
2020-11-17 00:22:48 -0300

    feat: add type player

---
+++ src/players/types/player.type.ts
@@ -0,0 +1,9 @@
+export type PlayerType = {
+  readonly _id: string
+  readonly phoneNumber: string
+  readonly email: string
+  readonly name: string
+  readonly ranking: string
+  readonly rankingPosition: number
+  readonly urlPhotoPlayer: string
+}

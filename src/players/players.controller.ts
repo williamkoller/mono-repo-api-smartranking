b7db6d242f552dc2642181b7573463708e192c9e
William Koller <wkoller25@gmail.com>
2020-11-26 22:21:42 -0300

    feat: add get player

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,13 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Get, Post } from '@nestjs/common'
 import { CreatePlayerDTO } from './dto/create-player.dto'
+import { PlayersService } from './players.service'
+import { Player } from './types/player.type'
 
 @Controller('api/v1/players')
 export class PlayersController {
-  @Post()
-  async createUpdatePlayer(@Body() createPlayerDTO: CreatePlayerDTO): Promise<CreatePlayerDTO> {
-    const { email } = createPlayerDTO
-    return {
-      email,
+    constructor(private readonly playersService: PlayersService) {}
+
+    @Post()
+    async createUpdatePlayer(@Body() createPlayerDTO: CreatePlayerDTO): Promise<void> {
+        await this.playersService.createUpdatePlayer(createPlayerDTO)
     }
-  }
+
+    @Get()
+    async consultPlater(): Promise<Player[]> {
+        return this.playersService.consultAllPlayer()
+    }
 }
--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
 import { PlayersController } from './players.controller'
+import { PlayersService } from './players.service'
 
 @Module({
   controllers: [PlayersController],
+  providers: [PlayersService],
 })
 export class PlayersModule {}

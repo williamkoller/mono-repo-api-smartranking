536ac9b7019d22be86e420f58424ebe2e13234eb
William Koller <wkoller25@gmail.com>
2020-11-26 23:04:01 -0300

    fix: remove params readonly

--- src/players/types/player.type.ts
+++ src/players/types/player.type.ts
@@ -2,8 +2,8 @@ export type Player = {
     readonly _id: string
     readonly phoneNumber: string
     readonly email: string
-    readonly name: string
-    readonly ranking: string
-    readonly rankingPosition: number
-    readonly urlPhotoPlayer: string
+    name: string
+    ranking: string
+    rankingPosition: number
+    urlPhotoPlayer: string
 }

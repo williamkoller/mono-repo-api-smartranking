d22766cd0a5466f081016c194ce2bdab349c7e71
william koller <wkoller25@gmail.com>
2020-12-08 17:36:03 -0300

    feat: exports PlayersService

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -15,5 +15,6 @@ import { PlayerSchema } from './schema/player.schema'
     ],
     controllers: [PlayersController],
     providers: [PlayersService],
+    exports: [PlayersService],
 })
 export class PlayersModule {}

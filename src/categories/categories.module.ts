e3072c009c669441f547c4995c9d6cae9c457774
william koller <wkoller25@gmail.com>
2020-12-12 15:16:15 -0300

    feat: set CategoriesService

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -17,5 +17,6 @@ import { CategorySchema } from './schema/category.schema'
   ],
   controllers: [CategoriesController],
   providers: [CategoriesService],
+  exports: [CategoriesService],
 })
 export class CategoriesModule {}

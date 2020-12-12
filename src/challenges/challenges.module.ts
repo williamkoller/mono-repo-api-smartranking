0e11fe7c9bedd96d03b471f4a2bc8287f2b66b87
william koller <wkoller25@gmail.com>
2020-12-12 15:16:28 -0300

    feat: add MongooseModule

--- src/challenges/challenges.module.ts
+++ src/challenges/challenges.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common'
 import { ChallengesService } from './challenges.service'
 import { ChallengesController } from './challenges.controller'
+import { MongooseModule } from '@nestjs/mongoose'
+import { ChallengeSchema } from './schema/challenge.schema'
+import { MatchSchema } from './schema/match.schema'
+import { PlayersModule } from 'src/players/players.module'
+import { CategoriesModule } from 'src/categories/categories.module'
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Challenge',
+        schema: ChallengeSchema,
+      },
+      {
+        name: 'Match',
+        schema: MatchSchema,
+      },
+    ]),
+    PlayersModule,
+    CategoriesModule,
+  ],
   providers: [ChallengesService],
   controllers: [ChallengesController],
 })

0e0bff58250392667b6ec55440a9b0a1b22e683e
william koller <wkoller25@gmail.com>
2020-12-13 23:37:46 -0300

    refactor: remove document

--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -9,11 +9,6 @@ import { ReturnTypeResponse } from './types/return.type.response'
 export class PlayersService {
   constructor(@InjectModel('Player') private readonly playerModel: Model<Player>) {}
 
-  /**
-   * @param {CreatePlayerDTO} createPlayerDto
-   * @return {*}  {Promise<Player>}
-   * @memberof PlayersService
-   */
   async createPlayer(createPlayerDto: CreatePlayerDTO): Promise<Player> {
     const { email } = createPlayerDto
     const playerFound = await this.playerModel.findOne({ email }).exec()
@@ -24,12 +19,6 @@ export class PlayersService {
     return await playerCreated.save()
   }
 
-  /**
-   * @param {string} _id
-   * @param {UpdatePlayerDTO} updatePlayerDto
-   * @return {*}  {Promise<Player>}
-   * @memberof PlayersService
-   */
   async updatePlayer(_id: string, updatePlayerDto: UpdatePlayerDTO): Promise<Player> {
     const playerFound = await this.playerModel.findOne({ _id }).exec()
     if (!playerFound) {
@@ -38,21 +27,18 @@ export class PlayersService {
     return await this.playerModel.findOneAndUpdate({ _id }, { $set: updatePlayerDto }).exec()
   }
 
-  /**
-   * @return {*}  {(Promise<Player[] | CreatePlayerDTO[]>)}
-   * @memberof PlayersService
-   */
   async searchForAllPlayer(): Promise<Player[]> {
-    return await this.playerModel.find({}, { __v: false }).exec()
+    const players = await this.playerModel.find({}, { __v: false }).exec()
+
+    if (!players) {
+      throw new BadRequestException('There is no registered player.')
+    }
+
+    return players
   }
 
-  /**
-   * @param {string} _id
-   * @return {*}  {}
-   * @memberof PlayersService
-   */
   async searchByPlayerId(_id: string): Promise<CreatePlayerDTO> {
-    const playerFound = await this.playerModel.findOne({ _id }, { __v: false }).exec()
+    const playerFound = await this.playerModel.findOne({ _id }).exec()
 
     if (!playerFound) {
       throw new NotFoundException(`Player not found.`)
@@ -68,13 +54,8 @@ export class PlayersService {
     return playerObject
   }
 
-  /**
-   * @param {string} _id
-   * @return {*}  {Promise<ReturnTypeResponse>}
-   * @memberof PlayersService
-   */
   async deletePlayerById(_id: string): Promise<ReturnTypeResponse> {
-    const playerFound = await this.playerModel.findOne({ _id }, { __v: false }).exec()
+    const playerFound = await this.playerModel.findOne({ _id }).exec()
 
     if (!playerFound) {
       throw new NotFoundException(`Player not found.`)

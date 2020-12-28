b58f92dba8f3075d26f52e4d1b211ba2e41a1002
william koller <wkoller25@gmail.com>
2020-12-27 21:37:54 -0300

    refactor: alter DB_URI

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { PlayersModule } from './players/players.module'
 import { CategoriesModule } from './categories/categories.module'
-import { ChallengesModule } from './challenges/challenges.module';
+import { ChallengesModule } from './challenges/challenges.module'
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://mongo_db/api-smartranking', {
+    MongooseModule.forRoot('mongodb://db_mongo:27017/api-smartranking', {
       useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true,

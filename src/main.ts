7920d868eefac90885a31aff8f5dc6d22788b1e8
william koller <wkoller25@gmail.com>
2020-12-27 23:45:23 -0300

    feat: add NestExpressApplication

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
 import { AppModule } from './app.module'
 import { HttpExceptionFilter } from './common/filters/http-exception.filter'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.useGlobalFilters(new HttpExceptionFilter())
   await app.listen(process.env.PORT)
 }

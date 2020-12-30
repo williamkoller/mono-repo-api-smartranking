be20570dbcd0983dd0950dda900442c1c054da13
william koller <wkoller25@gmail.com>
2020-12-29 21:11:09 -0300

    feat: add moment-timezone

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { AppModule } from './app.module'
 import { HttpExceptionFilter } from './common/filters/http-exception.filter'
+import * as momentTimezone from 'moment-timezone'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.useGlobalFilters(new HttpExceptionFilter())
+  Date.prototype.toJSON = (): any => {
+    return momentTimezone(this).tz('America/Sao_Paulo').format('YYYY-MM-DD HH:mm:ss.SSS')
+  }
   await app.listen(process.env.PORT)
 }
 

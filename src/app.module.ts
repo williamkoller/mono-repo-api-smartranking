716ae386bb5268e169f90af8fa8c6b7505f0bb37
william koller <wkoller25@gmail.com>
2021-01-28 01:29:06 -0300

    chore: add nest-morgan

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { PlayersModule } from './players/players.module'
 import { CategoriesModule } from './categories/categories.module'
 import { ChallengesModule } from './challenges/challenges.module'
+import { MorganInterceptor, MorganModule } from 'nest-morgan'
+import { APP_INTERCEPTOR } from '@nestjs/core'
 
 @Module({
   imports: [
+    MorganModule.forRoot(),
     MongooseModule.forRoot('mongodb://db_mongo:27017/api-smartranking', {
       useCreateIndex: true,
       useNewUrlParser: true,
@@ -17,6 +20,11 @@ import { ChallengesModule } from './challenges/challenges.module'
     ChallengesModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: MorganInterceptor('dev'),
+    },
+  ],
 })
 export class AppModule {}
